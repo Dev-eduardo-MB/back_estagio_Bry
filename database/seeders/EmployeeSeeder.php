260f259cfe56141b3e5ae8989a07478bd7acb867
Dev-eduardo-MB <eduardo.moreirabernardi@gmail.com>
2025-11-30 06:28:06 -0300

    Feat: Adiciona seeders de Company e Employee e ajusta DatabaseSeeder
    Inclui seeders para empresas e funcionários com associações automáticas e atualiza o DatabaseSeeder para executá-los.

---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Employee;
+use App\Models\Company;
+
+class EmployeeSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $companies = Company::all();
+
+        Employee::factory(1)->create()->each(function ($employee) use ($companies) {
+            $employee->companies()->attach(
+                $companies->random(rand(1, 3))->pluck('id')
+            );
+        });
+    }
+}
